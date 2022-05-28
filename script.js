e32210a1e9fee72d57a9b2c87430bfc68bc2248c
Nekitech <beliaew.nikita@mail.ru>
2022-05-28 18:06:33 +0300

    add tags

--- script.js
+++ script.js
@@ -11,26 +11,28 @@ const colors = {
 hexPages.forEach((elem) => {
     elem.addEventListener('click', function(e) {
         if(this.id == 'hexPage'){
-            console.log(this)
             let page = document.getElementById(`${elem.getAttribute('sector')}`)
 
             page.classList.add('active-page')
             page.style.backgroundColor = `${colors[elem.getAttribute('sector')]}`
-
+            setTimeout(() => [
+                page.style.backgroundColor = '#fff'
+            ], 500)
             let close = page.querySelector('.hexagon__page-close')
-            let text = page.querySelectorAll('.hexagon__text-page')
-
-            text.forEach((el) => {
+            let blocks = page.querySelectorAll('.hexagon__block')
+            close.style.display = 'block'
+            blocks.forEach((el) => {
                 el.style.transform = 'translateY(0)'
                 el.style.opacity = '100%'
             })
                 
             close.addEventListener('click', function(e) {
                 page.classList.remove('active-page')
-                text.forEach((el) => {
-                    el.style.transform = 'translateY(200%)'
+                blocks.forEach((el) => {
+                    el.style.transform = 'translateY(300%)'
                     el.style.opacity = '0'
                 })
+                close.style.display = 'none'
                 e.stopPropagation()
             })
         }
