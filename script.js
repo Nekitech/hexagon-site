73f899ab8421755cf29299ccd4b5fa6cbca3cb4c
Nekitech <beliaew.nikita@mail.ru>
2022-05-28 15:19:23 +0300

    add gerbs, popup

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-// let hexPages = document.querySelectorAll('#hexPage')
-// const colors = {
-//     'industry': '#2c92f2',
-//     'export': '#7fb7eb',
-//     'culture-tour': '#7febc2',
-//     'prom-potential': '#9b7feb',
-//     'science-city': '#00C9EF',
-//     'agro': '#5BCBA4',
-//     'science-potential': '#00CCCC'
-// }
-// hexPages.forEach((elem) => {
-//     elem.addEventListener('click', function(e) {
-        
-//         if(this.id == 'hexPage'){
-//             // console.log(this)
-//             let page = this.querySelector('.hexagon__page')
-//             page.classList.add('active-page')
-//             elem.classList.add('hexagon__item-active')
-//             page.style.backgroundColor = `${colors[elem.getAttribute('sector')]}`
+let hexPages = document.querySelectorAll('#hexPage')
+const colors = {
+    'industry': '#2c92f2',
+    'export': '#7fb7eb',
+    'culture-tour': '#7febc2',
+    'prom-potential': '#9b7feb',
+    'science-city': '#00C9EF',
+    'agro': '#5BCBA4',
+    'science-potential': '#00CCCC'
+}
+hexPages.forEach((elem) => {
+    elem.addEventListener('click', function(e) {
+        if(this.id == 'hexPage'){
+            console.log(this)
+            let page = document.getElementById(`${elem.getAttribute('sector')}`)
 
-//             let close = elem.querySelector('.hexagon__page-close')
-//             let text = page.querySelectorAll('.hexagon__text-page')
-//             text.forEach((el) => {
-//                 el.style.transform = 'translateY(0)'
-//                 el.style.opacity = '100%'
-//             })
-                
-           
-//             close.addEventListener('click', function(e) {
-                
-//                 page.classList.remove('active-page')
-//                 elem.classList.remove('hexagon__item-active')
+            page.classList.add('active-page')
+            page.style.backgroundColor = `${colors[elem.getAttribute('sector')]}`
 
-//                 text.forEach((el) => {
-//                     el.style.transform = 'translateY(200%)'
-//                     el.style.opacity = '0'
-//                 })
+            let close = page.querySelector('.hexagon__page-close')
+            let text = page.querySelectorAll('.hexagon__text-page')
+
+            text.forEach((el) => {
+                el.style.transform = 'translateY(0)'
+                el.style.opacity = '100%'
+            })
                 
-//                 e.stopPropagation()
-//             })
-//         }
-//     })
-// })
+            close.addEventListener('click', function(e) {
+                page.classList.remove('active-page')
+                text.forEach((el) => {
+                    el.style.transform = 'translateY(200%)'
+                    el.style.opacity = '0'
+                })
+                e.stopPropagation()
+            })
+        }
+    })
+})
