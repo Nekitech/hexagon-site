40f7e7562efa06da138afa913350f2814a81195a
Nekitech <beliaew.nikita@mail.ru>
2022-06-02 22:01:03 +0300

    industry start slide is done

--- public/js/main.js
+++ public/js/main.js
@@ -37,32 +37,12 @@ hexPages.forEach((elem) => {
     })
 })
 
-
-
-let popupTour = document.querySelectorAll('.startSlide__block')
-popupTour.forEach((elem) => {
+let popupNav = document.querySelectorAll('.startSlide__block')
+popupNav.forEach((elem) => {
     elem.addEventListener('click', function (e) {
         let popup = document.querySelector(`[popup=${this.id}]`)
         popup.classList.add('active-page')
         let close = popup.querySelector('.hexagon__page-close')
-        close.style.display = 'block'
-            
-        close.addEventListener('click', function(e) {
-            popup.classList.remove('active-page')
-            close.style.display = 'none'
-            e.stopPropagation()
-        })
-    })
-})
-
-let blocksPopup = document.querySelectorAll('.startSlide__block')
-
-blocksPopup.forEach((elem) => {
-    elem.addEventListener('click', function (e) {
-        let popup = document.querySelector(`[popup=${this.id}]`)
-        popup.classList.add('active-page')
-        let close = popup.querySelector('.hexagon__page-close')
-        
         close.style.display = 'block'
             
         close.addEventListener('click', function(e) {
