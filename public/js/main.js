b7772987e8f14ee3bd33194f9f40c932d8ca8c97
Nekitech <beliaew.nikita@mail.ru>
2022-06-03 03:29:45 +0300

    nothing interesting

--- public/js/main.js
+++ public/js/main.js
@@ -45,9 +45,8 @@ popupNav.forEach((elem) => {
         let popup = document.querySelector(`[popup=${this.id}]`)
         popup.classList.add('active-page')
         let close = popup.querySelector('.hexagon__page-close')
-        close.style.display = 'block'
-            
-        close.addEventListener('click', function(e) {
+        close.style.display = 'block' 
+        close.addEventListener('click', function(e) {   
             popup.classList.remove('active-page')
             close.style.display = 'none'
             e.stopPropagation()
